99ed4bcaabd316a9f717ec8b090b667337e8d750
Avinash Kumar <154955355+avinashkkkk@users.noreply.github.com>
2026-02-19 17:01:24 +0530

    C++

---
+++ C language comp/exercise.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    printf("Enter Value: ");
+    scanf("%d",&a);
+    printf(" The value of gutka is %d",a);
+    return 0;
+}
---
+++ C language comp/tut4.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int glo = 6;
+void sum(){
+    int a;
+    cout<< glo;
+    
+    }
+    int main() {
+        int glo = 9;
+        glo = 78;
+        int a=14;
+        float pi=3.14;
+        char c='d';
+        bool is_false = true;
+
+        sum();
+        cout<<glo <<is_false;
+        return 0;
+
+    }
